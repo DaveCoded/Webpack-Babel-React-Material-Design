5f950087b136868b4084192ff01f5fca958afb8c
David Bernhard <davidbernhard@live.com>
2019-06-24 22:07:44 +0100

    add babel and sass/css loaders

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,16 @@ module.exports = {
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
   devServer: { contentBase: path.join(__dirname, "src") },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ["babel-loader"] },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.(jpg|jpeg|png|gif|mp3|svg)$/, loaders: ["file-loader"] }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html")
